5aec0ca926374ad2b8a0ca93960123b83acec23c
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-12-27 18:10:47 +0900

    add: types.

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,17 @@
 // TODO: import types of unified.
-export default function remarkFrontmatter(a: any): any;
+import type { Node, TransformCallback } from "unified";
+import type { VFile } from "vfile";
+
+// remark transformer.
+export default function remarkFrontmatter<
+  Input extends Node = Node,
+  // Output extends Node = Input
+>(
+  node: Input,
+  file: VFile,
+  next: TransformCallback
+): Promise<any, any>;
+
+interface Frontmatter {
+  [key: string]: number | string;
+}
